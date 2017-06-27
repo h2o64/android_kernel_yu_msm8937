75ad9a193c31d2f8160d30ca9f134c4a2b0581dd
Louis Popi <theh2o64@gmail.com>
2017-06-27 20:45:14 +0200

    leds: leds-qpnp-flash: Turn on front flash only if camera 1 is used

--- drivers/leds/leds-qpnp-flash.c
+++ drivers/leds/leds-qpnp-flash.c
@@ -1249,6 +1249,7 @@ static void qpnp_flash_led_work(struct work_struct *work)
 	int rc, brightness;
 	int max_curr_avail_ma = 0;
 	int total_curr_ma = 0;
+	int camera_id = get_camera_id();
 	int i;
 	u8 val;
 
@@ -1257,7 +1258,7 @@ static void qpnp_flash_led_work(struct work_struct *work)
 
 	/* Global lock is to synchronize between the flash leds and torch */
 	mutex_lock(&led->flash_led_lock);
-	if (flash_node->id == FLASH_LED_FRONT) {
+	if (flash_node->id == FLASH_LED_FRONT && camera_id == 1) {
 		if (!brightness) {
 			if (gpio_is_valid(led->pdata->front_flash_gpio_en))
 				gpio_set_value(led->pdata->front_flash_gpio_en, 0);
