a90f335bebe6cd6cd8ce0774aba6f9209b4d4876
Louis Popi <theh2o64@gmail.com>
2017-10-20 21:10:17 +0200

    drivers: misc: Import Tinno's Hall Sensor
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -337,6 +337,10 @@
 
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
 
+#define KEY_HALLOPEN            249
+#define KEY_HALLCLOSE     	250
+
+
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
 #define BTN_MISC		0x100
